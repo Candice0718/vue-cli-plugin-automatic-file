7fb60d320a74faf1f7d5b1b2bfb76c978e36c5de
Candice <wuqiong910718@163.com>
2021-05-26 20:24:37 +0800

    组件自动注册

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ module.exports = (api, options) => {
                          * @desc 组件自动化注册
                          * @important 此文件禁止手动修改！
                          */
+                        import Vue from 'vue';
                         ${template}
                     `;
                 },
